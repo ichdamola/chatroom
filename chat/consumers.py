da81564e2f3572817ed1250eb0669f7ba2086ee1
Adedapo Adedamola <adedadamola@gmail.com>
2021-04-09 20:17:25 +0100

    [Feature] User sending msg capability

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,5 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,23 +13,42 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'hello world',
-            }
-        )
+    #     await self.channel_layer.group_send(
+    #         self.room_group_name,
+    #         {
+    #             'type': 'tester_message',
+    #             'tester': 'hello world',
+    #         }
+    #     )
 
-    async def tester_message(self, event):
-        tester = event['tester']
+    # async def tester_message(self, event):
+    #     tester = event['tester']
 
-        await self.send(text_data=json.dumps({
-            'tester': tester,
-        }))
+    #     await self.send(text_data=json.dumps({
+    #         'tester': tester,
+    #     }))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+    
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chatroom_message',
+                'message': message,
+            }
+        )
+    
+    async def chatroom_message(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+        }))
