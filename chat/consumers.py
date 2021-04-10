07a8ba18bbef503fea9f6a0ddf7a90e2a85375a3
Adedapo Adedamola <adedadamola@gmail.com>
2021-04-10 23:34:52 +0100

    [IMP] Set up the username to assign message to.

--- chat/consumers.py
+++ chat/consumers.py
@@ -13,21 +13,6 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-    #     await self.channel_layer.group_send(
-    #         self.room_group_name,
-    #         {
-    #             'type': 'tester_message',
-    #             'tester': 'hello world',
-    #         }
-    #     )
-
-    # async def tester_message(self, event):
-    #     tester = event['tester']
-
-    #     await self.send(text_data=json.dumps({
-    #         'tester': tester,
-    #     }))
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -37,18 +22,22 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = text_data_json['username']
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chatroom_message',
                 'message': message,
+                'username': username,
             }
         )
     
     async def chatroom_message(self, event):
         message = event['message']
+        username = event['username']
 
         await self.send(text_data=json.dumps({
             'message': message,
+            'username': username,
         }))
